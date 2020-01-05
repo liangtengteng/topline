f2327b4eb6118738bdfd2fabff125fec01734ba2
凉飕飕 <739092064@qq.com>
2020-01-05 18:07:18 +0800

    01project

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 引入elementui组件库
+import ElementUI from 'element-ui'
+// 引入css样式
+import 'element-ui/lib/theme-chalk/index.css'
+// 把组件库注册给vue
+// 现在组件注册：Vue.use(组件模块)一次性注册全部组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  lintOnSave: false
+  lintOnSave: true, // 文件保存时就做eslint规则检测
+  deServer: {
+    open: true, // 自动开启浏览器
+    port: 19988 // 服务运行端口
+  }
+
 }
